db0622ba096513dd8e2a0b0e478bd0805078b005
Churro3000 <churro25200@gmail.com>
2025-11-08 13:27:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Script loaded successfully");
+function scrollToSection(id) {
+  const section = document.getElementById(id);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
